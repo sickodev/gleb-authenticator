2a21ec8ead4c5aa232fd00ce02e7de97b06295f0
sickodev <kbishwa03kalyan@gmail.com>
2025-04-30 17:51:38 +0530

    Initial Commit

---
+++ src/main/java/com/gleb/glebauthenticator/GlebAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.gleb.glebauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlebAuthenticatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlebAuthenticatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gleb/glebauthenticator/GlebAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleb.glebauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlebAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
